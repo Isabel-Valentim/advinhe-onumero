0a9491c952093bd11248bb2c781d4298c6686361
Isabel <isabelvalentim18@gmail.com>
2022-07-12 22:47:41 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,52 +1,68 @@
-const res = document.querySelector('#res')
+const input = document.querySelector('#res')
 const resbtn = document.querySelector('#btnres')
 
-const palpitesInput = document.querySelector('.palpites')
+const palpites = document.querySelector('.palpites')
 const ultimoRes = document.querySelector('.ultimoRes')
 const baixoOuAlto = document.querySelector('.baixoOuAlto')
 
 /*Gerar um número aleatório entre 1 e 100*/
-let numAleatorio = Math.floor(Math.random() *100)+1
+var numAleatorio = Math.floor(Math.random() *100)+1
 
 /*Gravar o número do turno que o jogador está. Iniciar em 1.*/
-
-var contagemPalpites = 1;
-var palpiteValue = Number(palpitesInput.value)
+var contagemPalpites = 0;
 
 resbtn.addEventListener("click", analisePalpites)
 
 function analisePalpites(){
-    if(palpiteValue == NaN){
-        alert('Faça um palpite')
+    var res = input.value 
+    if(res != ""){
+        contagemPalpites = contagemPalpites + 1
+        console.log(res)
+        console.log(contagemPalpites)
+        console.log(numAleatorio)
+        /*Após a tentativa ter sido submetida, primeiro gravar em algum lugar para que o usuário possa ver as tentativas anteriores.*/
+        palpites.innerHTML += contagemPalpites+' - '+res+' / '
+        if (res == numAleatorio){
+            /*Se estiver correto: Escrever mensagem de parabéns.*/
+            ultimoRes.innerHTML = "Parabéns! Você acertou. Vamos jogar de novo"
+            ultimoRes.style.backgroundColor = 'green'
+            configFimDeJogo()
+        }else if(contagemPalpites == 10){
+            //Se o jogador está errado mas não tem turnos sobrando:
+            //Dizer ao jogador que o jogo acabou.
+            ultimoRes.innerHTML = "!!!Suas tentativas terminaram!!! Jogue Novamente!"
+            ultimoRes.style.backgroundColor = 'red'
+            configFimDeJogo()
+        }else{
+            // Se o palpite estiver errado e o jogador ainda tem turnos sobrando: Dizer ao jogador que ele está errado.
+            ultimoRes.innerHTML= 'Errado!'
+            ultimoRes.style.backgroundColor='red'
+            // Permitir que ele insira outra resposta.
+            input.value = ''
+            if(res < numAleatorio){
+                baixoOuAlto.innerHTML = 'Seu palpite está muito baixo'
+            } else if (res > numAleatorio){
+                baixoOuAlto.innerHTML = 'Seu palpite está muito alto'
+            }
+        }
     }else{
-        let palpite = palpiteValue
-        console.log(palpite)
+        alert('Faça um palpite')        
     }
+    
     
 }
 
-/*Após a tentativa ter sido submetida, primeiro gravar em algum lugar para que o usuário possa ver as tentativas anteriores.*/
+function configFimDeJogo(){
+    console.log('fim do jogo')
+    // Impedir que o jogador insira mais respostas (isso pode bugar o jogo).
+    contagemPalpites = 0
+    baixoOuAlto.innerHTML = ''
+    palpites.innerHTML = 'Seus Palpites:'
+    // Mostrar controle que permita ao jogador reiniciar o jogo.
+    alert('Essa partida terminou faça um novo palpite para recomeçar o jogo')
+    // Permitir que ele insira outra resposta.
+    input.value = ''
+}
 
 
-/*Se estiver correto:
-Escrever mensagem de parabéns.
 
-Impedir que o jogador insira mais respostas (isso pode bugar o jogo).
-
-Mostrar controle que permita ao jogador reiniciar o jogo.*/
-
-/*Se o palpite estiver errado e o jogador ainda tem turnos sobrando:
-Dizer ao jogador que ele está errado.
-
-Permitir que ele insira outra resposta.
-
-Incrementar o número do turno em 1.*/
-
-/*Se o jogador está errado mas não tem turnos sobrando:
-Dizer ao jogador que o jogo acabou.
-
-Impedir que o jogador insira mais respostas (isso pode bugar o jogo).
-
-Mostrar controle que permita ao jogador reiniciar o jogo.*/
-
-// Quando reiniciar, tenha certeza de resetar todas as variáveis e a interface do jogo, então volte para o passo 1.
